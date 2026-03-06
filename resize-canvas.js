90ac913c010d5d3ec6ee8005fea801a40c0a09a6
Stefan Musarra <stefan0@stefan0.com>
2026-03-06 00:05:03 -0800

    initial commit

---
+++ resize-canvas.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("unity-canvas");
+  const container = document.getElementById("unity-container");
+  // canvas.style.width = container.offsetWidth + "px";
+  // canvas.style.height = container.offsetHeight + "px";
+  canvas.style.width = "800px";
+  canvas.style.height = "450px";
+});
